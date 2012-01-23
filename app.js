94780ece916c2dd069209043c6487c14e0606b09
Arron Schaar <aschaar@gmail.com>
2012-01-23 15:17:56 -0800

    added *.css files

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 var express = require('express'),
-	fs = require('fs');
-
-var app = express.createServer();
-
-
+	fs = require('fs'),
+	path = require('path'),
+	app = express.createServer();
 
 app.get('/', function(req, res){
-    fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
+    fs.readFile(path.join(__dirname, 'index.html'), 'utf8', function(err, text){
         res.send(text);
     });
 });
 
-app.use(express.static(__dirname  + '/public'));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(1337);
