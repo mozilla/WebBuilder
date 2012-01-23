bc5594144d81dd2a5bd80bef26cf20ce2de8d768
Arron Schaar <aschaar@gmail.com>
2012-01-23 14:58:34 -0800

    fixed nodejs to server static resources

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var express = require('express'),
 
 var app = express.createServer();
 
+
+
 app.get('/', function(req, res){
     fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
         res.send(text);
     });
 });
+
+app.use(express.static(__dirname  + '/public'));
 
 app.listen(1337);
