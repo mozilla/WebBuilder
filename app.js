6ebf076d8ca26ff976406a68dce8f9b84ad9e3af
Arron Schaar <aschaar@gmail.com>
2012-01-23 14:26:52 -0800

    added node stuff

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+	fs = require('fs');
+
+var app = express.createServer();
+
+app.get('/', function(req, res){
+    fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+
+app.listen(3000);
